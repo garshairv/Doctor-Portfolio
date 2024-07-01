fc8ac657640a5932076b9653bdc8e5f0c6f365df
Garsha Iravani <iamirgarsha@gmail.com>
2024-07-01 12:31:27 +0330

    first commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
    {
-      title: 'Terminal Website',
-      imgUrl: '/assets/terminal-website.jpg',
-      stack: ['HTML', 'CSS', 'JavaScript'],
-      link: 'https://fkcodes.com',
+      title: 'Research Assistant at Cellular Biology Lab',
+      imgUrl: '/assets/clinic.jpeg',
+      stack: ['Cancer cell', 'Experiment', 'PubMed'],
+      link: 'https://google.com',
    },
    {
-      title: 'Developer Portfolio',
-      imgUrl: '/assets/dev-portfolio.jpg',
-      stack: ['React', 'Tailwind', 'Vite'],
-      link: 'https://thehackershideout.com',
+      title: 'Internship at XYZ Medical Center',
+      imgUrl: '/assets/clinic.jpeg',
+      stack: ['Intern', 'Clinic', 'microscopy'],
+      link: 'https://google.com',
    },
    {
-      title: 'AI Nerf Gun',
-      imgUrl: '/assets/ai-nerf-gun.jpg',
-      stack: ['Python', 'C++', 'Robotics'],
-      link: 'https://youtu.be/A8eegxL82PI',
+      title: 'Volunteer at ABC Community Health Clinic',
+      imgUrl: '/assets/clinic.jpeg',
+      stack: ['Volunteer', 'Clinic', 'Health'],
+      link: 'https://google.com',
    },
 ]
